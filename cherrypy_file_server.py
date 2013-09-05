4560c982c0cc64691d40f0a49f5d7bbccdbe442c
Pkpo <Pkpo@users.noreply.github.com>
2013-09-05 23:34:22 +0100

    Create cherrypy_file_server.py
    Main file

---
+++ cherrypy_file_server.py
@@ -0,0 +1,203 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Made by XaXa
+# 
+# version 0.1
+# 
+
+
+import os
+import socket,struct
+import cherrypy
+from cherrypy.lib.static import serve_file
+
+try:
+	import android
+	_droid_=android.Android()
+	_wifi_=_droid_.wifiGetConnectionInfo().result
+	print 'SSID : ' + _wifi_['ssid']
+	_ip_=_wifi_['ip_address']
+	_ip_=socket.inet_ntoa(struct.pack('L',_ip_))
+	print 'ip : ' +str(_ip_)
+except:
+	print 'not android machine'
+
+
+
+head="""
+<html>
+<head>
+<title>XaXa Explorer</title>
+"""
+
+
+
+css=""" 
+<style type="text/css">
+/* More info here : http://www.red-team-design.com/css3-ordered-list-styles */
+body {
+    margin: 40px auto;
+    width: 500px;
+}
+
+ol{
+	counter-reset: li; /* Initiate a counter */
+	list-style: none; /* Remove default numbering */
+	*list-style: decimal; /* Keep using default numbering for IE6/7 */
+	font: 15px 'trebuchet MS', 'lucida sans';
+	padding: 0;
+	margin-bottom: 4em;
+	text-shadow: 0 1px 0 rgba(255,255,255,.5);
+}
+
+ol ol{
+	margin: 0 0 0 2em; /* Add some left margin for inner lists */
+}
+
+.rounded-list a.file{
+	position: relative;
+	display: block;
+	padding: .4em .4em .4em 2em;
+	*padding: .4em;
+	margin: .5em 0;
+	background: #ddd;
+	color: #444;
+	text-decoration: none;
+	border-radius: .3em;
+	transition: all .3s ease-out;        
+}
+
+.rounded-list a.directory{
+	position: relative;
+	display: block;
+	padding: .4em .4em .4em 2em;
+	*padding: .4em;
+	margin: .5em 0;
+	#background: #EBB946;
+	background: #879DF5;
+	color: #444;
+	text-decoration: none;
+	border-radius: .3em;
+	transition: all .3s ease-out;        
+}
+
+.rounded-list a:hover{
+	background: #eee;
+}
+# .rounded-list a:hover:before{
+# transform: rotate(360deg); 
+# }
+
+.rounded-list a.directory:before{
+	content: counter(li);
+	counter-increment: li;
+	position: absolute;        
+	left: -1.3em;
+	top: 50%;
+	margin-top: -1.3em;
+	 #background: #D49504;
+	background: #788CDE;
+	height: 2em;
+	width: 2em;
+	line-height: 2em;
+	border: .3em solid #fff;
+	text-align: center;
+	font-weight: bold;
+	border-radius: 2em;
+	transition: all .3s ease-out;
+}
+.rounded-list a.file:before{
+	content: counter(li);
+	counter-increment: li;
+	position: absolute;        
+	left: -1.3em;
+	top: 50%;
+	margin-top: -1.3em;
+	background: #87ceeb;
+	height: 2em;
+	width: 2em;
+	line-height: 2em;
+	border: .3em solid #fff;
+	text-align: center;
+	font-weight: bold;
+	border-radius: 2em;
+	transition: all .3s ease-out;
+}
+</style>
+
+"""
+
+
+upload="""
+</head>
+<body>
+            <h2>Upload a file</h2>
+            <form action="/upload" method="post" enctype="multipart/form-data">
+            filename: <input type="file" name="myFile" /><br />
+            <input type="submit" />
+            </form>
+            <h2>Download a file</h2>
+<h2>{0}</h2>
+<ol class="rounded-list">
+"""
+
+hlist="""
+<li><a class='{0}' href='{1}'>{2}</a></li>
+
+"""
+foot="""
+</ol>
+</body>
+</html>
+"""
+
+
+
+_base_dir_=os.getcwd().split(os.path.sep)[0]+os.path.sep
+
+class Explore(object):
+
+	def default(self,*args):
+		temp_walk=os.path.join(_base_dir_,os.path.sep.join(args))
+		if os.path.isdir(temp_walk):
+			try:
+				templst=os.listdir(temp_walk)
+			except:
+				return 'impossible de lister le repertoire....'
+		elif os.path.isfile(temp_walk):
+			return serve_file(temp_walk,"application/x-download","attachement")
+		else:
+			return 'ni fichier ni repertoire....'
+		#templst.sort()
+		bod=''.join(sorted([hlist.format('directory','/'+'/'.join(args+(x,)),x) \
+									if os.path.isdir('/'+'/'.join(args+(x,))) \
+									else hlist.format('file','/'+'/'.join(args+(x,)),x) \
+								for x in templst]))
+
+		return head+css+upload.format(temp_walk)+bod+foot
+	default.exposed = True
+
+	def upload(self, myFile):
+		out = """<html>
+	<body>
+		myFile length: %s<br />
+		myFile filename: %s<br />
+		myFile mime-type: %s
+		</body>
+	</html>"""
+		size = 0
+		while True:
+			data = myFile.file.read(8192)
+			if not data:
+				break
+			size += len(data)
+		return out % (size, myFile.filename, myFile.content_type)
+	upload.exposed = True
+
+
+
+cherrypy.server.socket_host="0.0.0.0"
+#cherrypy.config.update({'log.screen':False})
+if __name__ == '__main__':
+	cherrypy.quickstart(Explore())
